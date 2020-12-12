1708055fcfa948ba61983eaac990ad5fcb3d388b
beyoungV <43118622+beyoungV@users.noreply.github.com>
2020-12-12 21:51:15 +0800

    Update gorilla-mux.go

--- golang-projects/go-web-server/examples/gorilla_mux/gorilla-mux.go
+++ golang-projects/go-web-server/examples/gorilla_mux/gorilla-mux.go
@@ -19,6 +19,21 @@ func(w http.ResponseWriter, r *http.Request) {
 
 
 
+//将请求处理程序限制为特定的HTTP方法。
+r.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+
+
 
 //将请求处理程序限制为特定的主机名或子域。
 r.HandleFunc("/books/{title}", BookHandler).Host("www.mybookstore.com")
+
+
+//将请求处理程序限制为http / https。
+r.HandleFunc("/secure", SecureHandler).Schemes("https")
+r.HandleFunc("/insecure", InsecureHandler).Schemes("http")
+
+
+
