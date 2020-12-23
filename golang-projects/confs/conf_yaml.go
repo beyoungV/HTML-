b9a70c416b6adc55d3b484735457260965832f46
beyoungV <43118622+beyoungV@users.noreply.github.com>
2020-12-23 12:18:58 +0800

    Create conf_yaml.go

---
+++ golang-projects/confs/conf_yaml.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+
+    "gopkg.in/yaml.v2"
+)
+
+type conf struct {
+    Enabled bool   `yaml:"enabled"` //yaml：yaml格式 enabled：属性的为enabled
+    Path    string `yaml:"path"`
+}
+
+func (c *conf) getConf() *conf {
+    yamlFile, err := ioutil.ReadFile("conf.yaml")
+    if err != nil {
+        log.Printf("yamlFile.Get err   #%v ", err)
+    }
+
+    err = yaml.Unmarshal(yamlFile, c)
+    if err != nil {
+        log.Fatalf("Unmarshal: %v", err)
+    }
+    return c
+}
+
+func main() {
+    var c conf
+    c.getConf()
+    fmt.Println("path:" + c.Path)
+}
