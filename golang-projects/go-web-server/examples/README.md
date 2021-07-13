c32c48da20f03025ddfcb33e757299aa79e88045
beyoungV <43118622+beyoungV@users.noreply.github.com>
2021-07-14 01:19:44 +0800

    Delete golang-projects directory

--- golang-projects/go-web-server/examples/README.md
+++
@@ -1,6 +0,0 @@
-$ go run advanced-middleware.go
-
-$ curl -s http://localhost:8080/
-
-$ curl -s -XPOST http://localhost:8080/
-
--- golang-projects/go-web-server/examples/httpServer.go
+++
@@ -1,3 +0,0 @@
-http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Welcome to my website!")
-})
